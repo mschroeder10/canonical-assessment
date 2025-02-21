e3d1968ffab43a6d371a1d59f34db69be780fd46
mschroeder11 <mschroeder@heilatech.com>
2025-02-21 10:47:14 -0700

    nkjhtf

--- src/main/main.py
+++ src/main/main.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python3
+"""A program to display information from a Debian Contents file.
+
+It determines the top 10 packages that have the most files associated with them.
+
+"""
+
 import argparse
 from collections import Counter
 import gzip
@@ -5,32 +12,30 @@ import re
 import requests
 import sys
 
-URL_BASE = f'http://ftp.uk.debian.org/debian/dists/stable/main/'
+URL_BASE = 'http://ftp.uk.debian.org/debian/dists/stable/main/'
+
 
 def package_repr(pkgs: list) -> None:
-    """
-    pretty print packages and their frequencies
+    """Pretty print packages and their frequencies.
 
     ----
-    input 
+    input
     pkgs: list of tuples w/ package name and occurence frequency: list
     ----
     returns: None
     ----
     """
-    result = ""
-    max_len = max(len(i[0]) for i in pkgs) + 4 # set standard line length so pkg counts line up
+    # set standard line length so pkg counts line up
+    max_len = max(len(i[0]) for i in pkgs) + 4
     for i, p in enumerate(pkgs, start=1):
         print(f'{i:2}. {p[0]:{max_len}}\t{p[1]}')
-    print (result)
 
 
 def package_parser(arch: str, count: int = 10) -> list:
-    """
-    Gather statistics of the top number packages that have the most files associated with them. Default is 10
+    """Gather statistics of the top number packages that have the most files associated with them.
 
     ----
-    input: 
+    input:
     arch: architecture to get: str
     count: number of packages to get: int
     ----
@@ -41,7 +46,7 @@ def package_parser(arch: str, count: int = 10) -> list:
     url = f'{URL_BASE}Contents-{arch}.gz'
 
     # download content from debian website
-    try: 
+    try:
         req = requests.get(url, stream=True)
         req.raise_for_status()
 
@@ -49,36 +54,39 @@ def package_parser(arch: str, count: int = 10) -> list:
         data = req.raw
         with gzip.open(data, 'rb') as f:
             lines = f.readlines()
-            
+
         # decode data to utf-8 and put all pkgs into list
-        for l in lines:
-            match = re.match(r'^(.*)\s+(\S+)$', l.decode('utf-8').strip())
+        for line in lines:
+            match = re.match(r'^\s*(\S.*\S)\s+(\S+)\s*$', line.decode('utf-8').strip())
             if match:
                 file_path, current_pkgs = match.groups()
                 pkg_count.update(current_pkgs.split(','))
-        
-        #count top <count> packages. 
+
+        # count top <count> packages.
         top_x = pkg_count.most_common(count)
         return top_x
-    except Exception as e: # handle issues with requests itself
-        print(f"Content not found at {url}. Please double check the architecture exists at {URL_BASE}.")
-        print (e)
+    except Exception as e:  # handle issues with requests itself
+        print(f"Content not found at {url}.")
+        print(f"Please double check the architecture exists at {URL_BASE}.")
+        print(e)
         sys.exit(1)
 
+
 def main():
+    """Run the application."""
     parser = argparse.ArgumentParser(description='Debian top 10 packages per architecture')
-    parser.add_argument(dest="architecture", help="Enter a Debian architecture, ex. amd64, arm64, mips etc. Usage: python main.py amd64")
+    parser.add_argument(dest="architecture",
+                        help="Debian architecture name, ex. amd64, arm64, mips.")
     args = parser.parse_args()
     if args.architecture:
         arch = args.architecture
     else:
         print("Please pass in an architecture, for example amd64, arm64, mips etc.")
         sys.exit(0)
-    
 
-    pkgs = package_parser(arch) # call function to download & parse data
-    package_repr(pkgs) # pretty print data
-    
+    pkgs = package_parser(arch)  # call function to download & parse data
+    package_repr(pkgs)  # pretty print data
+
 
 if __name__ == "__main__":
     sys.exit(main())
